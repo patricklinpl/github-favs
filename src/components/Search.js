aeb0381b25a89328a5cbc1540b060973367597f9
Patrick Lin <patrick@patricklin.ca>
2018-09-21 12:44:57 -0400

    search bar without css

---
+++ src/components/Search.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Search extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { query: '' }
+    this.setQuery = this.setQuery.bind(this)
+  }
+
+  setQuery (event) {
+    this.setState({ query: event.target.value })
+  }
+
+  render () {
+    return (
+      <div className='form'>
+        <form onSubmit={this.props.handleSearch(this.state)}>
+          <div>
+            <input type='text' id='searchBar' value={this.state.query} onChange={this.setQuery} required />
+            <input className='btn' type='submit' value='Submit' />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+Search.propTypes = {
+  handleSearch: PropTypes.func.isRequired
+}
