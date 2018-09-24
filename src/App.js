f218bd44e4d8c4a1c0c6527a9881c8ff742b7481
Patrick Lin <patrick@patricklin.ca>
2018-09-24 07:03:28 -0400

    remove react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './containers/Home'
 
 const App = () => (
-  <BrowserRouter>
-    <div className='container'>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route render={() => <p>404: Page Not Found</p>} />
-      </Switch>
-    </div>
-  </BrowserRouter>
+  <div className='container'>
+    <Home />
+  </div>
 )
 
 export default App
