d2dcc924babdb9eb5efb8420240639eabbd0f782
Patrick Lin <patrick@patricklin.ca>
2018-10-10 00:42:03 -0400

    rm firebase & add url path 'shopify'

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use('/shopify', express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/shopify', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(9000);
+
+console.log(path.join(__dirname, 'build'))
